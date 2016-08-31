f8cdc3fd7c6dd047982ef6eb2f822554ca61815a
fjfdszj <fjfdszj@163.com>
2016-08-31 08:39:22 +0800

    服务端提交事件

--- SHU.K3.SCM.PlugInEx/SHU.K3.SCM.PlugInEx/Submit.cs
+++ SHU.K3.SCM.PlugInEx/SHU.K3.SCM.PlugInEx/Submit.cs
@@ -39,14 +39,16 @@ namespace SHU.K3.SCM.PlugInEx.Bill.DELIVERYNOTICE
                     string srcbillno = "";
                     string arg = extendedDataEntity["BillNo"].ToString();
                     String flag = "0";
-                    DynamicObjectCollection source = (DynamicObjectCollection)extendedDataEntity["SAL_DELIVERYNOTICEFIN"];//明细实体名称
-                    long finfid=Convert.ToInt64(source[0]["id"]);
-                    string strSql = "select FBILLALLAMOUNT from T_SAL_DELIVERYNOTICEFIN where fid="+ finfid;
+                   // DynamicObjectCollection source = (DynamicObjectCollection)extendedDataEntity["SAL_DELIVERYNOTICEFIN"];//明细实体名称
+                   // long finfid=Convert.ToInt64(source[0]["id"]);
+                    string strSql = "select FBILLALLAMOUNT from T_SAL_DELIVERYNOTICEFIN where fid="+ fid;
                     Decimal amt = DBUtils.ExecuteScalar<Decimal>(ctx, strSql, 0, null);;//当前金额
                     Decimal srcamt = new Decimal(0.00);//当前金额
                     Decimal auditamt = new Decimal(0.00);//当前金额
                     //1.查询销售合同总额及fid;
                     StringBuilder stringBuilder = new StringBuilder();
+                    //方案一:
+                    /*
                     stringBuilder.AppendLine(" select distinct main.FBILLNO,main.FID,fin.F_WDS_SUPAMOUNT,main.F_WDS_Iscredit from T_CRM_CONTRACT main ");
                     stringBuilder.AppendLine(" left outer join T_CRM_CONTRACTFIN fin on main.FID=fin.FID");
                     stringBuilder.AppendLine(" left outer join  T_CRM_CONTRACTENTRY sec on main.FID=sec.FID");
@@ -55,6 +57,13 @@ namespace SHU.K3.SCM.PlugInEx.Bill.DELIVERYNOTICE
                     stringBuilder.AppendLine("   and FTID in(SELECT FSID FROM t_BF_InstanceEntry WHERE (FSTableName = 'T_SAL_ORDERENTRY') and (FTTABLENAME='T_SAL_DELIVERYNOTICEENTRY') ");
                     stringBuilder.AppendLine(string.Format("and ftid in(select FENTRYID from T_SAL_DELIVERYNOTICEENTRY where FID={0}))", fid));
                     stringBuilder.AppendLine(" ) ");
+                    */
+                    stringBuilder.AppendLine(" select distinct main.FBILLNO,main.FID,fin.F_WDS_SUPAMOUNT,main.F_WDS_Iscredit from T_CRM_CONTRACT main ");
+                    stringBuilder.AppendLine(" left outer join T_CRM_CONTRACTFIN fin on main.FID=fin.FID");
+                    stringBuilder.AppendLine(" where main.FID in (");
+                    stringBuilder.AppendLine(string.Format(" select F_SHU_CONTRACTID from T_SAL_DELIVERYNOTICE where FID={0} ", fid));
+                    stringBuilder.AppendLine(" ) ");
+
 
                     using (IDataReader read=DBUtils.ExecuteReader(ctx, stringBuilder.ToString())) {
                         if (read.Read()) {
@@ -71,6 +80,8 @@ namespace SHU.K3.SCM.PlugInEx.Bill.DELIVERYNOTICE
                     }
                     //2.查询发货通知单及已经审核总额
                     stringBuilder = stringBuilder.Clear();
+                    //方案一:
+                    /*
                     stringBuilder.AppendLine(" select isnull(sum(FBillAllAmount),0) amt");
                     stringBuilder.AppendLine(" from(");
                     stringBuilder.AppendLine("   select distinct main.FID,fin.FBillAllAmount from T_SAL_DELIVERYNOTICE main ");
@@ -83,6 +94,17 @@ namespace SHU.K3.SCM.PlugInEx.Bill.DELIVERYNOTICE
                     stringBuilder.AppendLine(" ) ");
                     stringBuilder.AppendLine(" ) ");
                     stringBuilder.AppendLine(" )AAA ");
+                    */
+
+
+                    stringBuilder.AppendLine(" select isnull(sum(FBillAllAmount),0) amt");
+                    stringBuilder.AppendLine(" from(");
+                    stringBuilder.AppendLine("   select distinct main.FID,fin.FBillAllAmount from T_SAL_DELIVERYNOTICE main ");
+                    stringBuilder.AppendLine("   left outer join T_SAL_DELIVERYNOTICEFIN fin on main.FID=fin.FID");
+                    stringBuilder.AppendLine("   where main.FDOCUMENTSTATUS in('C','B')  ");
+                    stringBuilder.AppendLine(string.Format(" and main.F_SHU_CONTRACTID={0}", srcfid));
+                    stringBuilder.AppendLine(" )AAA ");
+
 
                     using (IDataReader read = DBUtils.ExecuteReader(ctx, stringBuilder.ToString()))
                     {
@@ -95,7 +117,7 @@ namespace SHU.K3.SCM.PlugInEx.Bill.DELIVERYNOTICE
                     //3.当前金额+已经审核 与合同金额比较.
                     if (srcamt.CompareTo(auditamt+ amt)==-1)
                     {
-                            validateContext.AddError(null, new ValidationErrorInfo("", fid.ToString(), extendedDataEntity.DataEntityIndex, extendedDataEntity.RowIndex, "DeliveryNotice500", string.Format(ResManager.LoadKDString("发货通知单价税合计超过销售合同供货金额,销售合同单号{0}，金额{1},已经确认金额{2}，本次确认金额{3}！", "004103000014337", SubSystemType.SCM, new object[0]), srcbillno,srcamt ,auditamt,amt), "", ErrorLevel.Error));
+                            validateContext.AddError(null, new ValidationErrorInfo("", fid.ToString(), extendedDataEntity.DataEntityIndex, extendedDataEntity.RowIndex, "DeliveryNotice500", string.Format(ResManager.LoadKDString("发货通知单价税合计超过销售合同供货金额,销售合同单号{0}，金额{1},已经确认金额{2}，本次确认金额{3}！", "004103000014337", SubSystemType.SCM, new object[0]), srcbillno, Math.Round(srcamt, 2).ToString(), Math.Round(auditamt, 2).ToString(), Math.Round(amt, 2).ToString()), "", ErrorLevel.Error));
                     }
                 }
             }
